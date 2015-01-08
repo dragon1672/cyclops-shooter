e1522f3468335fab846fa781f347e0bd4e100462
Anthony Corbin <dragon1672@gmail.com>
2015-01-08 09:43:41 -0700

    auto converted to tabs

--- CyclopsShooter/Assets/RailPointSystem/Scripts/RailPointManager.cs
+++ CyclopsShooter/Assets/RailPointSystem/Scripts/RailPointManager.cs
@@ -5,25 +5,29 @@ using System.Linq;
 
 public class RailPointManager : MonoBehaviour
 {
-    public GameObject HolderOfMoveToScript;
-    public GameObject HolderOfLookAtScript;
-    public string TagNameToGrabRailPoints = "RailPoint";
-    public float Delay = 0.0f;
-    public bool LoopPath = false;
+	public GameObject HolderOfMoveToScript;
+	public GameObject HolderOfLookAtScript;
+	public string TagNameToGrabRailPoints = "RailPoint";
+	public float Delay = 0.0f;
+	public bool LoopPath = false;
 
-    public bool AtEndRailPoint { get { return (_currentIndex >= _allRailPoints.Length); } }
-    private int _currentIndex;
-    private GameObject _point;
-    private GameObject[] _allRailPoints;
+	public bool AtEndRailPoint
+	{
+		get { return (_currentIndex >= _allRailPoints.Length); }
+	}
+
+	private int _currentIndex;
+	private GameObject _point;
+	private GameObject[] _allRailPoints;
 	public bool _moving { get; private set; }
 	private bool isInitialized = false;
 
-    // Use this for initialization
-    void Start()
-    {
-        _allRailPoints = GameObject.FindGameObjectsWithTag(TagNameToGrabRailPoints);
-        _allRailPoints = _allRailPoints.OrderBy(n => GetIndexFromRailPointName(n.name)).ToArray();
-    }
+	// Use this for initialization
+	void Start()
+	{
+		_allRailPoints = GameObject.FindGameObjectsWithTag(TagNameToGrabRailPoints);
+		_allRailPoints = _allRailPoints.OrderBy(n => GetIndexFromRailPointName(n.name)).ToArray();
+	}
 
 	public void Init()
 	{
@@ -40,87 +44,90 @@ public class RailPointManager : MonoBehaviour
 		StartCoroutine(WaitTillGo(Delay));
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-	    if (!enabled) return;
-	    if (!isInitialized)
-	    {
-		    Init();
-	    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (!enabled) return;
+		if (!isInitialized)
+		{
+			Init();
+		}
 		if (!_moving) return;
-	    UpdateCurrentIndex();
-	    UpdateMoveAbility();
-	    HolderOfLookAtScript.GetComponent<LookAtTarget>().CanUpdate = _moving;
-	    HolderOfMoveToScript.GetComponent<MoveToTarget>().CanUpdate = _moving;
-    }
+		UpdateCurrentIndex();
+		UpdateMoveAbility();
+		HolderOfLookAtScript.GetComponent<LookAtTarget>().CanUpdate = _moving;
+		HolderOfMoveToScript.GetComponent<MoveToTarget>().CanUpdate = _moving;
+	}
 
-    private void UpdateCurrentIndex()
-    {
-        _point = _allRailPoints[_currentIndex];
-        RailPoint rp = _point.GetComponent<RailPoint>();
+	private void UpdateCurrentIndex()
+	{
+		_point = _allRailPoints[_currentIndex];
+		RailPoint rp = _point.GetComponent<RailPoint>();
 
-        if (rp.HasBeenReached)
-        {
-            // Debug.Log("Index reached: " + currentIndex);
-            _currentIndex++;
-        }
-    }
-    private void UpdateMoveAbility()
-    {
-        if (AtEndRailPoint)
-        {
-            if (LoopPath)
-            {
-                _currentIndex = 0;
-            }
-            else
-            {
-                _moving = false;
-            }
-        }
-        else
-        {
-            SetUpNextPathTo();
-        }
-    }
-    private void SetUpNextPathTo()
-    {
-       GameObject nextLookAt = GetNextLookAt();
-       HolderOfLookAtScript.GetComponent<LookAtTarget>().TargetToLookAt = nextLookAt;
-       HolderOfMoveToScript.GetComponent<MoveToTarget>().targetToMoveTo = _point;
-    }
-    private GameObject GetNextLookAt()
-    {
-        _point = _allRailPoints[_currentIndex];
-        RailPoint rp = _point.GetComponent<RailPoint>();
-        GameObject nextLookAt = rp.ToLookAtWhenInRouteToMe;
-        if (nextLookAt == null)
-        {
-            int pointIndex = _currentIndex;
-            nextLookAt = _allRailPoints[pointIndex];
-        }
-        return nextLookAt;
-    }
+		if (rp.HasBeenReached)
+		{
+			// Debug.Log("Index reached: " + currentIndex);
+			_currentIndex++;
+		}
+	}
 
-    private IEnumerator WaitTillGo(float time)
-    {
-        yield return new WaitForSeconds(time);
-        _moving = true;
-    }
+	private void UpdateMoveAbility()
+	{
+		if (AtEndRailPoint)
+		{
+			if (LoopPath)
+			{
+				_currentIndex = 0;
+			}
+			else
+			{
+				_moving = false;
+			}
+		}
+		else
+		{
+			SetUpNextPathTo();
+		}
+	}
 
-    int GetIndexFromRailPointName(string src)
-    {
-        int ret = 0;
-        int power = 1;
-        for (int i = src.Length - 1; i >= 0; i--)
-        {
-            if ('0' <= src[i] && src[i] <= '9')
-            {
-                ret += ((int)src[i] - (int)'0') * power;
-                power *= 10;
-            }
-        }
-        return ret;
-    }
+	private void SetUpNextPathTo()
+	{
+		GameObject nextLookAt = GetNextLookAt();
+		HolderOfLookAtScript.GetComponent<LookAtTarget>().TargetToLookAt = nextLookAt;
+		HolderOfMoveToScript.GetComponent<MoveToTarget>().targetToMoveTo = _point;
+	}
+
+	private GameObject GetNextLookAt()
+	{
+		_point = _allRailPoints[_currentIndex];
+		RailPoint rp = _point.GetComponent<RailPoint>();
+		GameObject nextLookAt = rp.ToLookAtWhenInRouteToMe;
+		if (nextLookAt == null)
+		{
+			int pointIndex = _currentIndex;
+			nextLookAt = _allRailPoints[pointIndex];
+		}
+		return nextLookAt;
+	}
+
+	private IEnumerator WaitTillGo(float time)
+	{
+		yield return new WaitForSeconds(time);
+		_moving = true;
+	}
+
+	private int GetIndexFromRailPointName(string src)
+	{
+		int ret = 0;
+		int power = 1;
+		for (int i = src.Length - 1; i >= 0; i--)
+		{
+			if ('0' <= src[i] && src[i] <= '9')
+			{
+				ret += ((int) src[i] - (int) '0')*power;
+				power *= 10;
+			}
+		}
+		return ret;
+	}
 }
