bda4f96b9452b079dbec7d322e99137076c9a7c2
Brittany Waite <bnwbnw3@gmail.com>
2015-01-11 22:37:30 -0700

    Now made it so that when enemys are hit with the laser they disapear(Anthony code) and spawn an explosion(My code)

--- CyclopsShooter/Assets/Scripts/LaserBeamScript.cs
+++ CyclopsShooter/Assets/Scripts/LaserBeamScript.cs
@@ -6,10 +6,14 @@ public class LaserBeamScript : MonoBehaviour {
     public ParticleSystem particleSystem;
     public string ButtonToFireLaser = "Fire1";
 
+    public GameObject destroyEffectObject;
+    private float destroyParicleAfter = 1.0f;
+
     public float Speed{ get;set;}
 	// Use this for initialization
 	void Start () {
         particleSystem.enableEmission = false;
+        destroyEffectObject.SetActive(false);
         Speed = 40f;
         //Turn Mouse Off
         Screen.lockCursor = true;
@@ -32,6 +36,15 @@ public class LaserBeamScript : MonoBehaviour {
         {
            //keep particle system going
             particleSystem.startSpeed = Speed;
+            RaycastHit hitter;
+            Physics.Raycast(transform.parent.transform.position, transform.parent.transform.forward, out hitter, 10000);
+            if (hitter.collider != null && hitter.collider.GetComponent<CyclopsEnemy>() != null)
+            {
+                destroyEffectObject.SetActive(true);
+                GameObject enemy = hitter.collider.gameObject;
+                Destroy(Instantiate(destroyEffectObject, enemy.transform.position, enemy.transform.rotation), destroyParicleAfter);
+                enemy.SetActive(false);
+            }
             yield return null;
         }
         particleSystem.enableEmission = false;
