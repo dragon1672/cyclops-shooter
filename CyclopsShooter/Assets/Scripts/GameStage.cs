815f6b9581b605cc5fa2565b11e9d15b39acd7f3
Anthony Corbin <dragon1672@gmail.com>
2015-01-15 10:08:41 -0700

    Swapping to using rail point manager instead of colliders

--- CyclopsShooter/Assets/Scripts/GameStage.cs
+++ CyclopsShooter/Assets/Scripts/GameStage.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 public class GameStage : MonoBehaviour
 {
-	public ColliderNotify MyTrigger;
+	public RailPointManager PreceedingRailPointManager;
+
 	private CyclopGameManager _myManager;
 	private bool _activeInGame = false;
 	private CyclopsEnemy[] _players;
@@ -13,18 +14,14 @@ public class GameStage : MonoBehaviour
 	{
 		_players = GetComponentsInChildren<CyclopsEnemy>();
 		_myManager = GetComponentInParent<CyclopGameManager>();
-		MyTrigger.OnCollidionAction += OnCollisionEnter;
-	}
-
-	void OnCollisionEnter(Collision collision)
-	{
-		if (collision.gameObject.GetComponentInChildren<CyclopsMainPlayer>() == null) return;
-
-		_activeInGame = true;
-		foreach (var cyclopsEnemy in _players)
+		PreceedingRailPointManager.CompletedEvent += () =>
 		{
-			cyclopsEnemy.GameStarted();
-		}
+			_activeInGame = true;
+			foreach (var cyclopsEnemy in _players)
+			{
+				cyclopsEnemy.GameStarted();
+			}
+		};
 	}
 
 	// Update is called once per frame
@@ -33,6 +30,7 @@ public class GameStage : MonoBehaviour
 		if (!_activeInGame) return;
 		if (_players.All(n => n.IsDead))
 		{
+			Debug.Log("All Dead");
 			_activeInGame = false;
 			_myManager.Continue();
 		}
