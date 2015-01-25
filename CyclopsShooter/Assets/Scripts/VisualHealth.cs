d2138d89ddcfe0378f24ceadb5f561e3abeee4ee
Brittany Waite <bnwbnw3@gmail.com>
2015-01-25 16:02:59 -0700

    Visual representation of health/getting shot with blood on the screen.

---
+++ CyclopsShooter/Assets/Scripts/VisualHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualHealth : MonoBehaviour {
+
+    private CanvasGroup healthCanvas;
+    public float Alpha{get;set;}
+	// Use this for initialization
+	void Start () {
+        healthCanvas = gameObject.GetComponent<CanvasGroup>();
+        Alpha = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (healthCanvas.alpha != Alpha)
+        {
+            healthCanvas.alpha = Alpha;
+        }
+	}
+}
