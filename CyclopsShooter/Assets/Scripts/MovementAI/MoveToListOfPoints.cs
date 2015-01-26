b9425fb80e8488a2407309687250b6db67857614
Anthony Corbin <dragon1672@gmail.com>
2015-01-26 10:40:27 -0700

    Adding scripts to stay and shoot at location, and move to array of points (compared to random)

---
+++ CyclopsShooter/Assets/Scripts/MovementAI/MoveToListOfPoints.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToListOfPoints : AINode
+{
+	public AINode[] nodes;
+	private int CurrentIndex = 0;
+	public float MovePercentBoost = 1;
+	public float AnglePercentBoost = 1;
+
+
+	public override void EnterAction(CyclopsEnemy character, AINode previousMovementPoint)
+	{
+		AINode toMoveTo = nodes[CurrentIndex++];
+		CurrentIndex %= nodes.Length;
+		StartCoroutine(MoveToNewPointSpeed(character, toMoveTo, character.MovementSpeed * MovePercentBoost, character.AngleSpeed * AnglePercentBoost));
+	}
+}
---
+++ CyclopsShooter/Assets/Scripts/MovementAI/StandAndShoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StandAndShoot : AINode {
+
+	public float MinTimeBetweenShoot = .1f;
+	public float MaxTimeBetweenShoot = .5f;
+
+	public override void EnterAction(CyclopsEnemy character, AINode previousMovementPoint)
+	{
+		StartCoroutine(ShootLoop(character));
+	}
+
+	private IEnumerator ShootLoop(CyclopsEnemy character)
+	{
+		while (!character.IsDead)
+		{
+			float timeTillShoot = Random.Range(MinTimeBetweenShoot, MaxTimeBetweenShoot);
+			yield return new WaitForSeconds(timeTillShoot);
+			character.Shoot();
+		}
+	}
+}
