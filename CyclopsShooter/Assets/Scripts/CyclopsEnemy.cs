b95cde390099182ac25772884976886508395261
Anthony Corbin <dragon1672@gmail.com>
2015-01-26 09:25:03 -0700

    Removing Time logic from enemy

--- CyclopsShooter/Assets/Scripts/CyclopsEnemy.cs
+++ CyclopsShooter/Assets/Scripts/CyclopsEnemy.cs
@@ -7,21 +7,18 @@ public class CyclopsEnemy : LaserHittable
 {
 	public GameObject VisualGameObject = null;
 	public AudioClip ShootAudioClip;
-	public float MinTimeBetweenShots;
-	public float MaxTimeBetweenShots;
 	public GameObject DestroyEffectObject;
     public AINode CurrentAIPoint;
+
+	public float MovementSpeed = 2;
+	public float AngleSpeed = 30;
 
 	/// <summary>
 	/// how long to wait after GameStart has been called
 	/// </summary>
 	public float DelayToStart = 0;
 
-	private float _currentDelay;
 	private bool _activeInGame = false;
-
-	private float _timeSinceLastShot = 0;
-	private float _nextShotTime = -1;
 
     void Start()
     {
@@ -30,16 +27,13 @@ public class CyclopsEnemy : LaserHittable
 
 	public void GameStarted()
 	{
-		_activeInGame = true;
-		_currentDelay = DelayToStart;
-		_timeSinceLastShot = 0;
-		_nextShotTime = Random.Range(MinTimeBetweenShots, MaxTimeBetweenShots);
 		StartCoroutine(StartMovement(DelayToStart));
 	}
 
 	private IEnumerator StartMovement(float waitTime)
 	{
 		yield return new WaitForSeconds(waitTime);
+		_activeInGame = true;
 		gameObject.SetActive(true);
         CurrentAIPoint.EnterAction(this,null); // init AI
 	}
@@ -48,27 +42,11 @@ public class CyclopsEnemy : LaserHittable
 	private void Update()
 	{
 		if (!_activeInGame) return;
-
-		if (_currentDelay >= 0)
-		{
-			_currentDelay -= Time.deltaTime;
-			return;
-		}
-
-		if (_timeSinceLastShot > MinTimeBetweenShots)
-		{
-			if (_timeSinceLastShot >= _nextShotTime)
-			{
-				_timeSinceLastShot = 0;
-				_nextShotTime = Random.Range(MinTimeBetweenShots, MaxTimeBetweenShots);
-				Shoot();
-			}
-		}
-		_timeSinceLastShot += Time.deltaTime;
 	}
 
-	private void Shoot()
+	public void Shoot()
 	{
+		Debug.Log("Shooting");
 		//GetComponent<AudioManager>().PlayClip(ShootAudioClip);
 		//random chance to hit player
 		//BroadcastMessage("EnemyFiredShot");
