cdfa5336190f70368cbe652d244b6eb06f0261b9
Anthony Corbin <dragon1672@gmail.com>
2015-01-26 09:26:05 -0700

    Using character move speed and a multiplier from script

--- CyclopsShooter/Assets/Scripts/MovementAI/BasicMove.cs
+++ CyclopsShooter/Assets/Scripts/MovementAI/BasicMove.cs
@@ -5,12 +5,13 @@ using System;
 public class BasicMove : AINode
 {
     public AINode to;
-    public float MoveSpeed = 2;
-    public float AngleSpeed = 180;
+	public float MovePercentBoost = 1;
+	public float AnglePercentBoost = 1;
+	public float Delay = 0;
 
     public override void EnterAction(CyclopsEnemy character, AINode previousMovementPoint)
     {
-        StartCoroutine(MoveToNewPointSpeed(character, to, MoveSpeed, AngleSpeed));
+        StartCoroutine(MoveToNewPointSpeed(character, to, character.MovementSpeed * MovePercentBoost, character.AngleSpeed * AnglePercentBoost,Delay));
     }
 
     public override void ExitAction(CyclopsEnemy character) { }
--- CyclopsShooter/Assets/Scripts/MovementAI/MoveToRandomPoint.cs
+++ CyclopsShooter/Assets/Scripts/MovementAI/MoveToRandomPoint.cs
@@ -3,14 +3,13 @@
 public class MoveToRandomPoint : AINode
 {
     public AINode[] To;
-    public float MoveSpeedLow = 1;
-    public float MoveSpeedHigh = 2;
-    public float AngleSpeedLow = 180;
-    public float AngleSpeedHigh = 360;
+	public float MovePercentBoost = 1;
+	public float AnglePercentBoost = 1;
+
     public override void EnterAction(CyclopsEnemy character, AINode previousMovementPoint)
     {
 	    AINode toMoveTo = To[Random.Range(0, To.Length)];
-		StartCoroutine(MoveToNewPointSpeed(character, toMoveTo, Random.Range(MoveSpeedLow, MoveSpeedHigh), Random.Range(AngleSpeedLow, AngleSpeedHigh)));
+		StartCoroutine(MoveToNewPointSpeed(character, toMoveTo, character.MovementSpeed * MovePercentBoost, character.AngleSpeed * AnglePercentBoost));
     }
 
     public override void ExitAction(CyclopsEnemy character) { }
