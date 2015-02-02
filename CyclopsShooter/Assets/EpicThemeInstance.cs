8a328478db1f76a9948493d4225a8547daa048df
Brittany Waite <bnwbnw3@gmail.com>
2015-02-01 21:41:28 -0700

    made a prefab so that sound can begin in main menu and continue to exist

---
+++ CyclopsShooter/Assets/EpicThemeInstance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EpicThemeInstance : MonoBehaviour {
+
+    private GameObject themeMusic;
+	// Use this for initialization
+	void Start () {
+
+        if (themeMusic == null)
+        {
+            themeMusic = this.gameObject;
+            DontDestroyOnLoad(themeMusic);
+        }
+        else
+        {
+            DestroyImmediate(this.gameObject);
+        }
+	}
+}
