c393e9707b21de94f96a77391256a21d79e6fdf0
Brittany Waite <bnwbnw3@gmail.com>
2015-02-01 22:52:25 -0700

    Main Menu set up with better looking colors. Moved some files around

---
+++ CyclopsShooter/Assets/Scripts/EpicThemeInstance.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EpicThemeInstance : MonoBehaviour {
+
+    private static GameObject themeMusic;
+	// Use this for initialization
+	void Start () {
+
+        if (themeMusic == null)
+        {
+            themeMusic = this.gameObject;
+            themeMusic.audio.Play();
+            DontDestroyOnLoad(themeMusic);
+        }
+        else
+        {
+            DestroyImmediate(this.gameObject);
+        }
+	}
+}
---
+++ CyclopsShooter/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGame : MonoBehaviour {
+
+    public string UnityGameName;
+	// Use this for initialization
+    public void LoadTheGame()
+    {
+        Application.LoadLevel(UnityGameName); 
+    }
+}
