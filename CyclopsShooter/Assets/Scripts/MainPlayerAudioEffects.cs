9f8ba77dcc984d4264678309c660929d2f06a867
Anthony Corbin <dragon1672@gmail.com>
2015-02-01 19:24:56 -0700

    Adding more audio assets

---
+++ CyclopsShooter/Assets/Scripts/MainPlayerAudioEffects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CyclopsMainPlayer))]
+public class MainPlayerAudioEffects : MonoBehaviour
+{
+	private CyclopsMainPlayer player;
+	// Use this for initialization
+	void Start ()
+	{
+		player = GetComponent<CyclopsMainPlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
