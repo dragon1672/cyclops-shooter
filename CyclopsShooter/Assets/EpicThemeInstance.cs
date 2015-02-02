306586b3b6e4e59cc0431c32f703a5b3dee2582a
Brittany Waite <bnwbnw3@gmail.com>
2015-02-01 22:00:40 -0700

    forgot to put static on a variable

--- CyclopsShooter/Assets/EpicThemeInstance.cs
+++ CyclopsShooter/Assets/EpicThemeInstance.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EpicThemeInstance : MonoBehaviour {
 
-    private GameObject themeMusic;
+    private static GameObject themeMusic;
 	// Use this for initialization
 	void Start () {
 
