bfa812abfa0250511d04af181ff3379bd5b940c9
Anthony Corbin <dragon1672@gmail.com>
2015-02-01 23:08:24 -0700

    Adding movement multiplier on leaning

---
+++ CyclopsShooter/Assets/Scripts/MovementExaggerate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementExaggerate : MonoBehaviour
+{
+	public Vector3 Maginification;
+	public Transform toBaseOffOf;
+	
+	void Update()
+	{
+		transform.localPosition = toBaseOffOf.localPosition;
+		transform.localPosition.Scale(new Vector3(Maginification.x-1,Maginification.y-1,Maginification.z-1));
+	}
+}
