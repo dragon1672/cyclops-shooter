847392698c9135bc1de02e1553cd708f4ea6e97b
Brittany Waite <bnwbnw3@gmail.com>
2015-02-04 10:36:17 -0700

    Text on Screen has a script that will enable and disable the different text.

---
+++ CyclopsShooter/Assets/TextOnScreenEnabler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextOnScreenEnabler : MonoBehaviour {
+    public GameObject winText;
+    public GameObject gameoverText;
+    public GameObject pleasewaitText;
+    public bool startWinTextEnabled, startGameoverTextEnabled, startPleasewaitTextEnabled;
+    public bool EnableWinText
+    {
+        set { winText.SetActive(value); }
+        get { return winText.activeInHierarchy; }
+    }
+    public bool EnableGameoverText
+    {
+        set { gameoverText.SetActive(value); }
+        get { return gameoverText.activeInHierarchy; }
+    }
+    public bool EnablePleasewaitText
+    {
+        set { pleasewaitText.SetActive(value); }
+        get { return pleasewaitText.activeInHierarchy; }
+    }
+        // Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //DEBUG/////////////////////////////////////////
+
+        EnableWinText = startWinTextEnabled;
+        EnableGameoverText = startGameoverTextEnabled;
+        EnablePleasewaitText = startPleasewaitTextEnabled;
+
+        ////////////////////////////////////////////////
+	
+	}
+}
